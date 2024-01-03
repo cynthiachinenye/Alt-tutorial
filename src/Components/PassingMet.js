d78bddddd0de20fc88c3a6b9d7a507ebbc4d0235
cynthiachinenye <cynthiachiagu@gmail.com>
2024-01-03 11:51:45 +0100

    new

--- src/Components/PassingMet.js
+++ src/Components/PassingMet.js
@@ -1,3 +1,18 @@
+import React, { useState } from 'react'
+
+const PassingMet = () => {
+    const [count,setCount] = useState(0)
+  return (
+    <div>
+    <h1>Count{count}</h1>
+    <button onClick={setCount(count+1)}>increment</button>
+    <button onClick={setCount(count-1)}>decrement</button>
+      
+    </div>
+  )
+}
+
+export default PassingMet
 
 
 
